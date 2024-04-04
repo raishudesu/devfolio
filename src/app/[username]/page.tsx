5965db48665d0762d8827ca61ac3bb4c543ca0fe
raishudesu <bacaltosbaryshnikov@gmail.com>
2024-04-04 16:05:19 +0800

    add initial user component | modify schemas

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -2,6 +2,7 @@
 
 import { getUserByUsernameUtil } from "@/utils/userUtils";
 import { useQuery } from "@tanstack/react-query";
+import Image from "next/image";
 
 const UserPage = ({ params }: { params: { username: string } }) => {
   const { username } = params;
@@ -12,9 +13,30 @@ const UserPage = ({ params }: { params: { username: string } }) => {
     refetchOnWindowFocus: false,
   });
 
-  console.log(data);
-
-  return <div>User page</div>;
+  if (!isFetching) {
+    console.log(data);
+  }
+  return (
+    <section>
+      User page
+      {isFetching ? "Loading..." : null}
+      {isSuccess ? (
+        <Image
+          width={120}
+          height={120}
+          src={data?.user.imageLink as string}
+          priority
+          alt="profile-picture"
+          className="border rounded-full"
+        />
+      ) : null}
+      <div>
+        <h2>
+          {data?.user.firstName} {data?.user.lastName}
+        </h2>
+      </div>
+    </section>
+  );
 };
 
 export default UserPage;
